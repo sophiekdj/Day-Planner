9073ac16b60d434740e766a0ce8fa97c1adad774
sophiekdj <s.dejong057@gmail.com>
2021-09-25 22:26:45 +0800

    Added save button to connect to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
+// variables
 var timeblockEl = $("#container");
 var dateDisplayEl = $("#currentDay");
+var saveBtn = $("button.saveBtn.rounded-right");
+
+// Timeblock text - corresponding variables
+var plannerContent = [];
+var getLocalStorageData = JSON.parse(localStorage.getItem("planner-items"));
+
+// If local storage data does not equal 0...
+if (getLocalStorageData !== null) {
+  plannerContent = getLocalStorageData;
+}
 
 // handle displaying the time
 function displayTime() {
@@ -7,3 +18,25 @@ function displayTime() {
   dateDisplayEl.text(rightNow);
 }
 displayTime();
+
+saveBtn.on("click", function (event) {
+  event.preventDefault();
+  var container = $(this).parent().parent();
+  var inputValue = container.find("textarea").val();
+  var inputId = container.find("textarea").attr("id");
+  var textObj = {
+    "input-id": inputId,
+    "input-value": inputValue,
+  };
+
+  if (textObj["input-value"] !== "") {
+    plannerContent.push(textObj);
+    localStorage.setItem("planner-items", JSON.stringify(plannerContent));
+  }
+});
+
+function init() {
+  localStorage.getItem("planner-items");
+  $("#9").textContent = "plannerContent";
+}
+init();
